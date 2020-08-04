acc20da5083ac120f486a50ea10545c8fbfa8c89
Alexandra <sandra.larrosa88@gmail.com>
2020-08-04 13:06:43 +0200

    version 1.0

---
+++ docs/precache-manifest.75607ed922bf02f2033858b405a6f278.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f99436114b833efd5f603125c24ec3c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b6c1b66504d16738a01f",
+    "url": "./static/css/main.2e883a01.chunk.css"
+  },
+  {
+    "revision": "de01ca795793626cf551",
+    "url": "./static/js/2.56ac9748.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.56ac9748.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6c1b66504d16738a01f",
+    "url": "./static/js/main.4a8775e0.chunk.js"
+  },
+  {
+    "revision": "b388ac9c340cc6e2817c",
+    "url": "./static/js/runtime-main.fa365cae.js"
+  },
+  {
+    "revision": "48d36fbfec5c14160165bc9f75f4af05",
+    "url": "./static/media/Simpsonfont.48d36fbf.ttf"
+  },
+  {
+    "revision": "5174e5da391d1964e2a21443bb63325e",
+    "url": "./static/media/wallpaperflare.com_wallpaper.5174e5da.jpg"
+  }
+]);
